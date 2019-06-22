3438b726f8eb970d434c771c8ec032cdfc4940c6
Patricia Garcia <patijoe33@gmail.com>
2019-06-22 18:44:17 +0200

    Add pages

---
+++ docs/precache-manifest.2adc47a5536a565105841827f9a96556.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d2f0d2126626870e7dce3618162fad2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f42896cf48e8ccc878e1",
+    "url": "./static/css/main.3d05874f.chunk.css"
+  },
+  {
+    "revision": "84057c60d25d071f516d",
+    "url": "./static/js/2.d95d2c4d.chunk.js"
+  },
+  {
+    "revision": "f42896cf48e8ccc878e1",
+    "url": "./static/js/main.5a7ee1cd.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
